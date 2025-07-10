58eeb420c7275ad1940078cf667b7e2345e48d4a
khushboo545567 <khushbooyadav6266763530@gmail.com>
2025-07-10 18:32:13 +0530

    Multilevel_inheritance

---
+++ Inheritance/Multilevel_inherit_Human/GrandParents.java
@@ -0,0 +1,19 @@
+// MULTILEVEL INHERITANCE
+
+public class GrandParents{
+    private String name;
+    private int age ;
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+    public int getage(){
+        return age;
+    }
+}
---
+++ Inheritance/Multilevel_inherit_Human/Test.java
@@ -0,0 +1,7 @@
+public class Test{
+    public static void main(String[]args){
+        Child c1 = new Child();
+        c1.setName("khushboo");
+        System.out.println(c1.getName());
+    }
+}
