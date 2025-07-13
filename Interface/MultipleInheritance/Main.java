04975789067bd25d39cc08d8c30c99f5f2756d74
khushboo545567 <khushbooyadav6266763530@gmail.com>
2025-07-13 20:33:21 +0530

    Multiple Inheritance

---
+++ Interface/MultipleInheritance/Main.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String[]args){
+        SmartPhone sm = new SmartPhone();
+        sm.takePhoto();
+        sm.call();
+    }
+}
---
+++ Interface/MultipleInheritance/SmartPhone.java
@@ -0,0 +1,8 @@
+public class SmartPhone implements Phone, Camera{
+    public void call(){
+        System.out.println(" is calling");
+    }
+    public void takePhoto(){
+        System.out.println("taking photo ");
+    }
+}
